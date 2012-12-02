6ddcad2154354e66a1dc04936911f3e6c7197ebd
bloodysnowx <genius@tcct.zaq.ne.jp>
2012-12-02 23:44:29 +0900

    first commit
    Signed-off-by: bloodysnowx <genius@tcct.zaq.ne.jp>

---
+++ HelloWorld/HelloWorld/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  HelloWorld
+//
+//  Created by Atsushi Iwasa on 2012/12/02.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
